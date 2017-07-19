cfafc0189b5df5e8708edb4a91e3b9cfd4cd281e
Abhijit N <abijit.navale@calderacap.com>
2017-07-19 10:32:09 +0530

    cap ad nginx config changed

--- cryptomover/config/deploy.rb
+++ cryptomover/config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid only for current version of Capistrano
 lock "3.8.2"
-server '54.169.119.236', port: 22, roles: [:app], primary: true
+server '54.254.245.88', port: 22, roles: [:web, :app], primary: true
 
 set :application, "cryptomover"
 set :repo_url, "git@gitlab.com:Cryptomover_Web_App/web-app.git"
-set :user, 'ubuntu'
+set :user, 'cmdeploy'
 set :puma_threads, [4, 16]
 set :puma_workers, 0
 
@@ -22,18 +22,18 @@ set :puma_workers, 0
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-set :pty, true
+# set :pty, true
 set :pty,             true
-set :use_sudo,        false
+set :use_sudo,  true
 set :stage,           :staging
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/ebs/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/Projects/CryptoMover/cryptomover_abhijit.pem) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
