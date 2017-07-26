07a70c0a559ff747ff92499ca438fcbcebcb6a24
Abhijit N <abijit.navale@calderacap.com>
2017-07-26 14:38:42 +0530

    Index of funds added at header location on home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
-  def index    
+  def index
+    @index_names=%w[CM3 CM10 CME CM3x2]
+    @index_values=%w[409.2 5,321.6 1,500.2 450.2]
+    @value_changes=%w[+1.8% -.05% -1.5% +3.6%]
   end
 
   def about    
