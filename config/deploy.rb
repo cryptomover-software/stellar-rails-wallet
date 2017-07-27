51c8b7f5342d7766b06db740bfd1e65fce4b52c1
daveliu <dave@liageren.com>
2017-07-27 15:08:43 +0800

    change puma gem version

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ set :puma_prune_bundler, true
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
