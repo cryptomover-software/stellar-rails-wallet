3ffd74ee57166e98166cc52bc1ddbd8074fa02b5
daveliu <dave@liageren.com>
2017-07-27 15:15:40 +0800

    change database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 set :puma_prune_bundler, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml" #, "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
