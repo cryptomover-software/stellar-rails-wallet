89b38f2e9bc245ef87cd6294e04ec5cf05a13136
Abhijit N <abijit.navale@calderacap.com>
2017-07-28 15:52:26 +0530

    our indexes data added and formatted

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def index
     @index_names = %w[CM3 CM10 CME CM3x2]
+    @index_desc = ["Most Liquid coins index", "Market Representative Index", "Emerging Coin Index", "2X CM3 Exposure Index"]
+    @index_yield = ["+ 317% YTD", "+350% YTD", "+50%(Since Jul 4, 2017)", "+13% (Since Jul 01, 2017)"]
     @index_values = %w[409.2 5,321.6 1,500.2 450.2]
     @value_changes = [+1.8, -0.5, -1.5, +3.6]
   end
