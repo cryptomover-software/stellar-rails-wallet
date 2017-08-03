b0e64d72f86a930b14038b84ea039d50eb2549fb
Abhijit N <abijit.navale@calderacap.com>
2017-08-03 11:12:46 +0530

    placeholder formula for cm3 index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,12 @@ class PagesController < ApplicationController
     @index_names = %w[CM3 CM10 CME CM3x2]
     @index_desc = ["Most Liquid coins index", "Market Representative Index", "Emerging Coin Index", "2X CM3 Exposure Index"]
     @index_yield = ["+ 317% YTD", "+350% YTD", "+50%(Since Jul 4, 2017)", "+13% (Since Jul 01, 2017)"]
-    @index_values = %w[409.2 5,321.6 1,500.2 450.2]
     @value_changes = [+1.8, -0.5, -1.5, +3.6]
+
+    quantities = {"B": 10, "E": 25, "L": 5}
+    current_prices = {"B": 200000, "E": 150000, "L": 50000}
+    @cm3_price = quantities.values.zip(current_prices.values).map { |x, y| x * y}.inject(:+)
+    @index_values = [@cm3_price, 321.61, 500.2, 450.2]
   end
 
   def about    
