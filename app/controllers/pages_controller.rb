1c3c3a6dcf0b62eb8ea01d572746b43f0b2997c9
dave.liu <dave.liu@cryptmover.com>
2017-10-05 11:51:33 +0800

    add steller wallet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -49,14 +49,89 @@ class PagesController < ApplicationController
   end
 
   def stellar_login
-    render :layout => "dashboard"
+    if session[:address].present?
+      redirect_to "/stellar_dashboard"
+    else
+      render :layout => "dashboard"
+    end
+  end
+
+  def login
+    begin
+      pair = Stellar::KeyPair.from_seed(params[:seed])
+      session[:address] = pair.address
+      session[:seed] = pair.seed
+      redirect_to "/stellar_dashboard"
+    rescue
+      flash[:notice] = "Invalid seed, please check"
+      redirect_to "/stellar_login"
+    end
+  end
+
+  def logout
+    session[:address] = session[:seed] = nil
+    redirect_to "/stellar_login"
   end
 
   def stellar_account
+    if session[:address].present? && session[:seed].present?
+     @address = session[:address]
+     @seed = session[:seed]
+    else
+      random = Stellar::KeyPair.random
+      session[:address] = @address = random.address
+      session[:seed] = @seed = random.seed
+    end
     render :layout => "dashboard"
   end
 
+  def stellar_subscribe
+    if request.post?
+      @account = Account.new(params.require(:account).permit(:email, :public_address, :seed))
+      @account.public_address = session[:address]
+      @account.seed = session[:seed]
+      if @account.save
+        flash[:notice] = "you subscribe success, we will send you 20 lumens."
+        # str = %Q(python3 -c 'from Stellar_token_issuer import *;Account_creation("GDNRWBDBEGMO7BSVOJ5DFLWXRUZDMTABUKBN6LRWJRREKYEFAOXEFZN2", "SAFZ4JAMYXLYHIDR2YNJ3XRAGBWOMMDXYAO7UWNKOHOREKFNDXYQI6NJ", 2)')
+        # result = `#{str}`
+        redirect_to "/stellar_account"
+      else
+        flash[:notice] =  "#{@account.errors.full_messages}, please try again"
+        render :layout => "dashboard"
+      end
+    else
+      render :layout => "dashboard"
+    end
+  end
+
   def stellar_dashboard
+    url = "https://horizon.stellar.org/accounts/#{session[:address]}"
+    response = HTTParty.get(url)
+    body = JSON.parse(response.body)
+    @balances = body['balances'] || []
     render :layout => "dashboard"
+  end
+
+  def stellar_send
+    #@asset_name = (params[:asset_name] == "lumens") ? "XLM" : params[:asset_name]
+    if params[:asset_name].blank? || params[:asset_name] == "lumens"
+      str = %Q(python3 -c 'from Stellar_token_issuer import *; Sending_lumen("#{session[:address]}", "#{session[:seed]}", "#{params[:address]}", #{params[:amount]})')
+    else
+      asset_name, asset_address = params[:asset_name].split("|")
+      str = %Q(python3 -c 'from Stellar_token_issuer import *;Sending_asset("#{asset_name}", "#{asset_address}", "#{session[:address]}", "#{session[:seed]}", "#{params[:address]}", #{params[:amount]})')
+    end
+    logger.info "===============================#{str}"
+    result = `#{str}`
+
+    logger.info "===============================#{result}"
+
+    if result.index("passed")
+      flash[:notice] = "Transaction passed"
+    else
+      flash[:notice] = "Transaction failed"
+    end
+
+    redirect_to "/stellar_dashboard"
+
   end
 end
