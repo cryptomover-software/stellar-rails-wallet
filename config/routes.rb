36a68323926b8f4362446e3e34ee740d0e153ac1
dave.liu <dave.liu@cryptmover.com>
2017-10-18 16:31:53 +0800

    fix a bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root 'pages#stellar_login'
   get 'about', to: 'pages#about'
   get 'dashboard', to: 'pages#dashboard'
   get 'trade', to: 'pages#trade'
