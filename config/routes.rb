fbd647cc8510b05ffcba7d8274f7fc1ed9cfe899
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-01-31 17:24:10 +0530

    removed unwanted routes and methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#stellar_login'
+
   get 'dashboard', to: 'pages#dashboard'
-  get 'trade', to: 'pages#trade'
-  get 'trade1', to: 'pages#trade1'
-  get 'trade2', to: 'pages#trade2'
-  get 'trade3', to: 'pages#trade3'
+
   get 'login', to: 'pages#login'
-  get 'signup', to: 'pages#signup'
+
   get 'forgot_password', to: 'pages#forgot_password'
 
   get 'stellar_login', to: 'pages#stellar_login'
@@ -18,7 +16,7 @@ Rails.application.routes.draw do
   get 'stellar_send', to: 'pages#stellar_send'
   post 'stellar_send', to: 'pages#stellar_send'
 
-  post 'login', to: 'pages#login'
+  # post 'login', to: 'pages#login'
   get 'logout', to: 'pages#logout'
 
   post '/shopify/checkout_create', to: 'shopify#checkout_create'
