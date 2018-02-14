23688bab61cab8956a30297bbad4fd9514d92eb4
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-02-14 16:18:12 +0530

    new wallet page updated with alerts and new routes for new accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
-  get 'forgot_password', to: 'pages#forgot_password'
-
-  get 'dashboard', to: 'wallets#dashboard'
-  get 'transactions', to: 'wallets#transactions'
 
   get 'login', to: 'wallets#login'
-  get 'stellar_account', to: 'wallets#stellar_account'
+  get 'logout', to: 'wallets#logout'
+  get 'forgot_password', to: 'pages#forgot_password'
+  # post 'login', to: 'pages#login'
+
+  get 'dashboard', to: 'wallets#dashboard'
   get 'wallet', to: 'wallets#index'
+
+  get 'transactions', to: 'wallets#transactions'
+
+  get 'account', to: 'wallets#stellar_account'
+  get 'new_account', to: 'wallets#new_account'
+  
   get 'stellar_subscribe', to: 'pages#stellar_subscribe'
   post 'stellar_subscribe', to: 'pages#stellar_subscribe'
+
   get 'stellar_send', to: 'wallets#stellar_send'
   post 'stellar_send', to: 'wallets#stellar_send'
-
-  # post 'login', to: 'pages#login'
-  get 'logout', to: 'wallets#logout'
 
   post '/shopify/checkout_create', to: 'shopify#checkout_create'
 end
