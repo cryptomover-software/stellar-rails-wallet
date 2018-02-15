1d555234508c601361ca14137ec4cb08cf8ee7a5
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-02-15 17:32:37 +0530

    hack fix for theme issues

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Cryptomover
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.assets.precompile += %w( dashboard.css dashboard.js )
+    # config.assets.precompile += %w( application.css application.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
