362edb35b26aae256b0aa6f07b033d94fd766fce
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-03-15 18:09:53 +0530

    removed all unwanted db migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926022844) do
-
-  create_table "accounts", force: :cascade do |t|
-    t.string "public_address"
-    t.string "seed"
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.string "author_type"
-    t.integer "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
-  create_table "checkouts", force: :cascade do |t|
-    t.string "params"
-    t.string "checkout_id"
-    t.string "email"
-    t.datetime "checkout_created_at"
-    t.float "total_price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
