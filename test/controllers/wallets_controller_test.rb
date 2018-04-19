b2a84080a5ce13914cb93a0734fe488dd58c7334
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-04-19 15:43:54 +0530

    login failure redirection test added

--- test/controllers/wallets_controller_test.rb
+++ test/controllers/wallets_controller_test.rb
@@ -4,9 +4,13 @@ class WalletsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test 'login_redirection' do
-    puts '--> Caling Login action'
+  test 'login_success_redirection' do
     get '/login', params: {public_key: 'GDZP53LPGV4LGIZVTIPDIYL2N6VC6YYUBSJE66AZTNC77F6K2CN3K4OO'}
     assert_redirected_to(controller: 'wallets', action: 'index')
+  end
+
+  test 'login_failure_redirection' do
+    get '/login', params: {public_key: 'GDZP53LPGV4LGIZVTIPDIYL2N6VC6YYUBSJE66AZTNC77F6K2CN3K'}
+    assert_redirected_to(controller: 'pages', action: 'index')
   end
 end
