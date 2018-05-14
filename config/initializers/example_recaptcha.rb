f0a34edef0192a7ded629df8422654ebd1deaf45
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-05-14 10:40:17 +0530

    recaptcah config added, gitignore updated

---
+++ config/initializers/example_recaptcha.rb
@@ -0,0 +1,11 @@
+#
+# Note:
+# Rename this file to 'recaptcha.rb'
+# Add Key values
+#
+Recaptcha.configure do |config|
+  config.site_key  = ''
+  config.secret_key = ''
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
