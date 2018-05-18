8a9502be762082975ee5f8d2f5891d4f80bf33d7
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-05-18 19:08:16 +0530

    sync db column, rake task added

--- app/controllers/federations_controller.rb
+++ app/controllers/federations_controller.rb
@@ -3,18 +3,10 @@ class FederationsController < ApplicationController
     
   end
 
-  def sync_with_federation_database(federation)
-    
-  end
-  handle_asynchronously :sync_with_federation_database
-
   def create
     federation = Federation.new(federations_params)
     federation.address = session[:address]
     federation.save!
-    # ToDo Move this syncing to background job
-    # and asynchronously
-    sync_with_federation_database(federation)
 
     redirect_to federation_account_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514111501) do
+ActiveRecord::Schema.define(version: 20180518133405) do
 
 
   create_table "federations", force: :cascade do |t|
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180514111501) do
     t.text "address", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "synced", default: false
     t.index ["username"], name: "index_federations_on_username", unique: true
   end
 end
