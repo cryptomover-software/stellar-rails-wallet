a9e1c24de3b1164eeff34b66715a14aa271f4092
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-08-31 17:07:58 +0530

    confirm email test on federation added

--- test/controllers/federations_controller_test.rb
+++ test/controllers/federations_controller_test.rb
@@ -4,7 +4,7 @@ class FederationsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "duplicate_federation_address_creation" do
+  test "duplicate_federation_address_on_create" do
     @username = 'abhijit@cryptomover.com'
     get login_path, params: {public_key:
                                'GDZP53LPGV4LGIZVTIPDIYL2N6VC6YYUBSJE66AZTNC77F6K2CN3K4OO'}
@@ -16,6 +16,30 @@ class FederationsControllerTest < ActionDispatch::IntegrationTest
     assert_not_equal @username, @response.body
   end
 
-  test "send_verification_email_on_federation_address_creation" do
+  # ToDo
+  # test "send_verification_email_on_federation_address_on_create" do
+  #   @username = 'test_federation'
+  #   get login_path, params: {public_key:
+  #                              'GDWU47CWKN3OARGCFZGVE3JQ3FOCRV4GDLUSUXYKSUKG74OXWHGNGDJV'}
+
+  #   post federations_path, params: { federation:
+  #                                      { username: @username}, format: 'js' }, xhr: true
+  #   assert_equal @username, @response.body
+  # end
+
+  test "confirm_email_with_valid_token" do
+    @token = 'zyxw'
+    get confirm_email_path, params: {token: @token}
+    @federation = Federation.where(email_confirmation_token: @token).first
+
+    assert @federation.email_confirmed
+  end
+
+  test "do_not_confirm_existing_email" do
+    @token = 'abcd'
+    get confirm_email_path, params: {token: @token}
+    @federation = Federation.where(email_confirmation_token: @token).first
+
+    assert_redirected_to root_path
   end
 end
