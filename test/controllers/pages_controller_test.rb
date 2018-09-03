73347e0777dccbe3401acc770c9ff495bfb3c26f
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-09-03 12:31:47 +0530

    test to check if agreement modal is shown in first visit

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,4 +4,18 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'agreemen_modal' do
+    @agreement_message = 'I hereby confirm that I am not U.S. citizen/permanent resident representing U.S citizen/permanent residents.'
+    get root_path
+    # Modal should be present in first visit
+    assert_select '#agreement-modal' do |elements|
+      elements.each do |element|
+        assert_select element, '.modal-body' do |es|
+          es.each do |e|
+            assert_select e, 'p', @agreement_message
+          end
+        end
+      end
+    end
+  end
 end
