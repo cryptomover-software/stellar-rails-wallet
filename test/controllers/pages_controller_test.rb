02d616547185055f13a344444cfc88c96f285826
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-09-03 12:41:24 +0530

    updated agreement modal test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,10 +4,10 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test 'agreemen_modal' do
+  test 'agreement_modal' do
     @agreement_message = 'I hereby confirm that I am not U.S. citizen/permanent resident representing U.S citizen/permanent residents.'
     get root_path
-    # Modal should be present in first visit
+    # Agreement modal should be present in first visit
     assert_select '#agreement-modal' do |elements|
       elements.each do |element|
         assert_select element, '.modal-body' do |es|
@@ -17,5 +17,9 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
         end
       end
     end
+    # Agreement modal should not be present on all subsequent visits.
+    get login_path, params: {public_key:
+                               'GDZP53LPGV4LGIZVTIPDIYL2N6VC6YYUBSJE66AZTNC77F6K2CN3K4OO'}
+    assert_select '#agreement-modal', count: 0
   end
 end
