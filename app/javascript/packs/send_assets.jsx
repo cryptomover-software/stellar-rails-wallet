1d4fcd51c3ec402e08bde6eb88f031c05dd0e57b
Abhijit Navale <abhijit.navale@cryptomover.com>
2018-11-13 16:51:14 +0530

    send asset form html added

--- app/javascript/packs/send_assets.jsx
+++ app/javascript/packs/send_assets.jsx
@@ -58,8 +58,84 @@ class SendAssets extends React.Component {
                 <div className="form-label">
                   Asset Type
                   <select name="asset_name" id="asset-type" className="form-control">
-                    {this.props.assets.map((x, y)=><option key={y}>{x}</option>)};
+                    {this.props.assets.map((x, y)=><option value={x}>{x}</option>)};
                   </select>
+                </div>
+                <div className="text-muted mt-1">
+                  Balance: &nbsp;
+                  <span id="available-balance">4.89919</span>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="form-label">
+                  Amount
+                  <input className="form-control" id="amount-to-send" name="amount" placeholder="Amount to Transfer" required="" type="number"/>
+                  <button className="btn btn-brown bt-lg mt-2" id="send-max">
+                    Send Maximum Allowed: 3.39918
+                  </button>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="form-label">
+                  Type your Secret Key
+                </div>
+                <input className="form-control" id="secret-seed" placeholder="Your Secret Key" required="" type="password"/>
+              </div>
+              Memo Type
+              <div className="form-check form-check-inline" id="memo-types">
+                <label className="radio-inline">
+                  <input className="form-check-input" name="memotype" type="radio" value="text"/>
+                  TEXT
+                </label>
+                <label className="radio-inline">
+                  <input className="form-check-input" name="memotype" type="radio" value="id"/>
+                  ID
+                </label>
+                <label className="radio-inline">
+                  <input className="form-check-input" name="memotype" type="radio" value="hash"/>
+                  HASH
+                </label>
+                <label className="radio-inline">
+                  <input className="form-check-input" name="memotype" type="radio" value="return"/>
+                  RETURN
+                </label>
+              </div>
+              <div className="form-group">
+                <div className="form-label">
+                  Memo
+                </div>
+                <input className="form-control" disabled="" id="memo" placeholder="Type Memo" type="text"/>
+              </div>
+              <div className="form-check">
+                <label className="form-check-label">
+                  <input className="form-check-input" id="fund-new-chk" name="fund-new" type="checkbox" value="fund-new"/>
+                  Fund New Account
+                </label>
+              </div>
+              <hr></hr>
+              <div className="form-check">
+                <label className="form-check-label">
+                  <input className="form-check-input" id="agree-terms-chk" name="agree-transfer-terms" type="checkbox" value="agree-terms"/>
+                  I agree to
+                  <a href="https://github.com/cryptomover-code/stellar-rails/blob/master/LICENSE">Terms &amp; Conditions.</a>
+                </label>
+              </div>
+              <div className="fee-prompt mb-2 mt-2 text-danger">
+                Stellar Network charges a transaction fee of 0.00001 XLM for each transaction
+              </div>
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col">
+                    <div className="text-right">
+                      <button className="btn btn-danger" id="send_money" type="button">
+                        Send Money
+                      </button>
+                      <button className="btn btn-brown" id="create-send-money-trx" type="button">
+                        Create Transaction
+                      </button>
+                      <a className="btn btn-brown" id="cancel-btn" href="/">Cancel</a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
